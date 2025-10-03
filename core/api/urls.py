97427c96d3a0aec57461ef3a3d9a62f863bd7684
linuxtusX5 <salvatusjoffrey@gmail.com>
2025-10-03 13:45:25 +0800

    Profile endpoint

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import register_view, login_view, logout_view
+from .views import register_view, login_view, logout_view, profile_view
 
 urlpatterns = [
     # Authentication endpoints
     path('auth/register/', register_view, name='register'),
     path('auth/login/', login_view, name='login'),
     path('auth/logout/', logout_view, name='logout'),
+    path('auth/profile/', profile_view, name='profile'),
+
 ]
