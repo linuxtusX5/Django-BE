7a75207242054ad35dee5db0392f03b8b0684a48
linuxtusX5 <salvatusjoffrey@gmail.com>
2025-10-03 11:07:38 +0800

    Fixed User Login Serializer

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import UserProfile
+from django.contrib.auth import authenticate
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     """Serializer for user registration"""
@@ -39,12 +40,12 @@ class UserLoginSerializer(serializers.Serializer):
         password = attrs.get("password")
 
         if username and password:
-            user = Authentication(username = username, password = password)
-        if not user:
-            raise serializers.ValidationError("Invalid credentials")
-        if not user.is_valid:
-            raise serializers.ValidationError("User account is disabled")
-        attrs['user'] = user
+            user = authenticate(username = username, password = password)
+            if not user:
+                raise serializers.ValidationError("Invalid credentials")
+            if not user.is_active:
+                raise serializers.ValidationError("User account is disabled")
+            attrs['user'] = user
         else:
             raise serializers.ValidationError("Must include username and password")
         
