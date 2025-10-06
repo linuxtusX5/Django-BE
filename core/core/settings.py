97e970cb8b119af186f41294bcbd7f783f6fb83d
linuxtusX5 <salvatusjoffrey@gmail.com>
2025-10-06 19:34:04 +0800

    REST_FRAMEWORK

--- core/core/settings.py
+++ core/core/settings.py
@@ -79,6 +79,18 @@ DATABASES = {
         }
     }
 }
+# REST Framework configuration
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
