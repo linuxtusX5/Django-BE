53a1c4169aea600224c9939d861597721671c93e
linuxtusX5 <salvatusjoffrey@gmail.com>
2025-10-13 17:52:04 +0800

    added corsheaders

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import register_view, login_view, logout_view, profile_view, CategoryListCreateView, CategoryDetailView
+from .views import register_view, login_view, logout_view, profile_view, CategoryListCreateView, CategoryDetailView, ItemListCreateView, ItemDetailView, MyItemView, dashboard_stats
 
 urlpatterns = [
     # Authentication endpoints
@@ -12,4 +12,11 @@ urlpatterns = [
     path('categories/', CategoryListCreateView.as_view(), name='category-list-create'),
     path('categories/<str:pk>/', CategoryDetailView.as_view(), name='category-detail'),
 
+    #Item endpoints
+    path('items/', ItemListCreateView.as_view(), name='item-list-create'),
+    path('items/<str:pk>/', ItemDetailView.as_view(), name='item-detail'),
+    path('my-items/', MyItemView.as_view(), name='my-items'),
+
+    # Dashboard
+    path('dashboard/stats/', dashboard_stats, name='dashboard-stats'),
 ]
--- core/core/settings.py
+++ core/core/settings.py
@@ -29,10 +29,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'api'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -94,6 +96,17 @@ REST_FRAMEWORK = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
+# CORS settings
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
